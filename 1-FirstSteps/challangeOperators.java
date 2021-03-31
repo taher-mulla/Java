db54e2a3461908984515f565c9baa120fe4d4bd8
taher-mulla <68025686+taher-mulla@users.noreply.github.com>
2021-03-31 18:28:14 +0530

    Update challangeOperators.java

--- 1-FirstSteps/challangeOperators.java
+++ 1-FirstSteps/challangeOperators.java
@@ -1,6 +1,10 @@
+/********************************************Challange Operators********************************************/
+
+
 public class operators{
 	public static void main(String[] args){
-		double first=20.00D, second=80.00D, sum;
+		//The number is followed by a 'D' to tell the compiler to consider the number as a double
+		double first=20.00D, second=80.00D, sum;	
 		sum = (first + second) * 100.00;
 		sum %=40.00D;
 		System.out.println("Remainder = "+sum);
