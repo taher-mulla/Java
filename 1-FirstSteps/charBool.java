172463f921522a0382d5f0da7a3ca8212dc92bd2
taher-mulla <68025686+taher-mulla@users.noreply.github.com>
2021-03-31 18:33:00 +0530

    Update charBool.java

--- 1-FirstSteps/charBool.java
+++ 1-FirstSteps/charBool.java
@@ -1,15 +1,14 @@
+/********************************************Character Variable********************************************/
+//	Char variables are kept in unicode for, hence you can use this to inatilize the variable
+
 public class charBool{
 	public static void main(String[] args){
 		char myChar = 'D';
-		char myUnicode = '\u0044';
+		char myUnicode = '\u0044';	//Char is beeing stored is 'D'
 		System.out.println("My Character is = " + myChar);
 		System.out.println("My Unicode Char 0044 = " + myUnicode);
-		char myUnicode1 = '\u00A9';
-		System.out.println("My Unicode Char 1 00A9 = " + myUnicode1);
-		
-		boolean boolT = true;
-		boolean boolF = false;
-		
+		char myUnicode1 = '\u00A9';	//this code gives a sybol
+		System.out.println("My Unicode Char 1 00A9 = " + myUnicode1);	
 		
 	}
 }
