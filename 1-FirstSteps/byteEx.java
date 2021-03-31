3ce5b631c9477ab77a67f9aee33495a28d7e1705
taher-mulla <68025686+taher-mulla@users.noreply.github.com>
2021-03-31 17:42:49 +0530

    Update byteEx.java

--- 1-FirstSteps/byteEx.java
+++ 1-FirstSteps/byteEx.java
@@ -1,6 +1,8 @@
+//        EXAMPLE OF BYTE DATA TYPE          
+
 public class byteEx{
    public static void main(String[] args){
-      byte min = Byte.MIN_VALUE;
+      byte min = Byte.MIN_VALUE;          //MIN AND AX VALUES OF BYTE ARE ASSIGNED TO THE VARIABLES
       byte max = Byte.MAX_VALUE;
       
       System.out.println("Min Byte = "+min);
